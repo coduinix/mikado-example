805fa3f6af14dcfe090d6cccbbb2d8c1ecc55a82
Hinse ter Schuur <582538+coduinix@users.noreply.github.com>
2020-03-10 12:13:31 +0100

    Remove csvFile getter and setter from App

--- src/main/java/com/coduinix/example/App.java
+++ src/main/java/com/coduinix/example/App.java
@@ -1,23 +1,12 @@
 package com.coduinix.example;
 
-import java.io.File;
-
 public class App {
-    private static File csvFile;
     private TextFormatter textFormatter;
 
     public void run(final TransactionStore transactionStore) {
         textFormatter = new TextFormatter(transactionStore);
         final var formattedSummary = textFormatter.formatSummary();
         System.out.println(formattedSummary);
-    }
-
-    public static void setCsvFile(File file) {
-        csvFile = file;
-    }
-
-    public static File getCsvFile() {
-        return csvFile;
     }
 
 }
