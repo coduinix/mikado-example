b5a87edeb879ed12016d1f599013a69b63db7786
Hinse ter Schuur <582538+coduinix@users.noreply.github.com>
2020-03-10 12:13:31 +0100

    Remove csvFile getter and setter from App

--- src/main/java/com/coduinix/example/Launcher.java
+++ src/main/java/com/coduinix/example/Launcher.java
@@ -6,9 +6,8 @@ public class Launcher {
     public static void main(String[] args) {
         try {
             var file = new File(args[0]);
-            App.setCsvFile(file);
             App app = new App();
-            app.run(new CsvTransactionStore(App.getCsvFile()));
+            app.run(new CsvTransactionStore(file));
         } catch (Exception e) {
             e.printStackTrace();
         }
